81a90ad1aeb71985e50b863309cedf008b9ad02b
maquinon1612 <60478676+maquinon1612@users.noreply.github.com>
2021-06-16 15:08:54 +0200

    regualrización y funciones basicas

--- Proyecto/regularización.py
+++ Proyecto/regularización.py
@@ -1,12 +1,9 @@
 from loader import *
+from fun_basicas import *
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.optimize as opt
-
-def sigmoide(Z):
-    sigmoide = 1 / (1 + np.exp(-Z))
-    return sigmoide
 
 def coste(Theta, X, Y):
     G = sigmoide(np.dot(X, Theta))
@@ -36,21 +33,6 @@ def gradiente_reg(Theta,X,Y,Lambda):
     gr = gradiente(Theta,X,Y)
     theta2 = (Lambda/m)*Theta
     return (gr + theta2)
-
-
-def preparaFunciones(Lambda):
-    c = lambda Theta, X, Y: coste_reg(Theta, X, Y, Lambda)
-    gr = lambda Theta, X, Y: gradiente_reg(Theta, X, Y, Lambda)
-
-    return (c, gr)
-
-
-def prepara_datos(X, y, et):
-    Y2 = (y == et) * 1
-    ## Aquí hay que hacer ravel de Y2 para pasar de (5000,1) a (5000,1)
-    ## y conseguir que funcione como en la practica anterior
-    Y2 = np.ravel(Y2)
-    return (X, Y2)
 
 
 def optimiza_reg(X, Y, Lambda, et):
